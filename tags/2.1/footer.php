33b2baf05167b3a7069d750e9a1183bb7e819c88
Ahmet Imamoglu <ahmet363@gmail.com>
2021-10-11 18:01:21 +0300

    Repo güncellendi.

--- tags/2.1/footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('AHMETI_WP_TIMELINE_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
-<br/>
-<br/>
-<p style="margin:30px 0 0 0 ;display:inline;font:oblique 11px/1em Georgia,serif"><?php echo _e('Developer','ahmeti-wp-timeline'); ?>:
-    <a target="_blank" href="http://ahmeti.net/">Ahmet İmamoğlu</a> |
-    <a target="_blank" href="http://ahmeti.net/ahmeti-wp-timeline-zaman-cizelgesi-eklentisi/"><?php echo _e('Plug-in Page','ahmeti-wp-timeline'); ?></a>
-</p>
-</div>
-
